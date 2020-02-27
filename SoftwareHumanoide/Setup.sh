4a03bd3516e742820b3fa9161dbce0ce9a0da3ae
Mark The Magnificent <mnegretev@gmail.com>
2020-02-26 18:48:09 -0600

    First draft of Practices Add Setup.sh for installation

---
+++ SoftwareHumanoide/Setup.sh
@@ -0,0 +1,6 @@
+sudo apt-get install ros-kinetic-gazebo-ros-control
+sudo apt-get install ros-kinetic-ros-controllers
+sudo apt-get install ros-kinetic-ros-control
+sudo apt-get install ros-kinetic-controller-manager
+sudo apt-get install ros-kinetic-effort-controllers
+sudo apt-get install ros-kinetic-joint-state-controller
